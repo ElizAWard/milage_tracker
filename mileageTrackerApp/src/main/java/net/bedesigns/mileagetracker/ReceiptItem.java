0017e01b15a9f4b0b455a1b07e3e23da6f84496b
Elizabeth Ward <softie0320@gmail.com>
2021-11-20 21:05:15 -0600

    updated receipt item

--- mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/ReceiptItem.java
+++ mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/ReceiptItem.java
@@ -6,7 +6,8 @@ import java.util.Calendar;
 public class ReceiptItem {
 //    private Instant date;
     private long date;
-    private int currentMileage;
+    private Integer currentMileage;
+    private Integer tripMiles;
     private double gasPurchased;
 
     public ReceiptItem(int currentMileage, double gasPurchased) {
@@ -14,8 +15,36 @@ public class ReceiptItem {
     }
 
     public ReceiptItem(Long date, int currentMileage, double gasPurchased) {
+        this(date, currentMileage, null, gasPurchased);
+    }
+
+    public ReceiptItem(Long date, Integer currentMileage, Integer tripMiles, double gasPurchased) {
         this.date = date;
         this.currentMileage = currentMileage;
+        this.tripMiles = tripMiles;
         this.gasPurchased = gasPurchased;
     }
+
+    public Integer getCurrentMileage() {
+        return currentMileage;
+    }
+
+    public double getGasPurchased() {
+        return gasPurchased;
+    }
+
+    public double getMilePerGallon(int previousMileage) {
+        double mileageDifference = currentMileage - previousMileage;
+        return mileageDifference / gasPurchased;
+    }
+
+    public double getMilesPerGallon() {
+        if (tripMiles != null) {
+            return tripMiles / gasPurchased;
+        } else {
+            return 0;
+        }
+    }
+
+
 }
