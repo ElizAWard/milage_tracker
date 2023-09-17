4269c0dcbc969926260f4aa3523b0e6a794453f9
Ben Ward <ben.g.ward@gmail.com>
2023-09-17 13:44:22 -0500

    Added non-null annotation to ReceiptItemEntity. Implemented RoomDatabase using an example.

--- mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/room/ReceiptItemEntity.java
+++ mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/room/ReceiptItemEntity.java
@@ -1,13 +1,18 @@
 package net.bedesigns.mileagetracker.room;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 
 import java.util.Date;
 
 @Entity(tableName = "Fill_ups", primaryKeys = {"vehicleName", "currentMileage"})
 public class ReceiptItemEntity {
+    @NonNull
     public String vehicleName;
+    @NonNull
     public int currentMileage;
+    @NonNull
     public Date date;
+    @NonNull
     public double gasPurchased;
 }
