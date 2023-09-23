3c744969030e9ce679ff70d25450202264baf66b
Ben Ward <ben.g.ward@gmail.com>
2023-09-22 21:40:09 -0500

    Room database fixes.

--- mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/room/ReceiptItemDatabase.java
+++ mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/room/ReceiptItemDatabase.java
@@ -7,11 +7,13 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {ReceiptItemEntity.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class ReceiptItemDatabase extends RoomDatabase {
     public abstract ReceiptItemDao ReceiptItemDao();
 
--- mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/room/ReceiptItemEntity.java
+++ mileageTrackerApp/src/main/java/net/bedesigns/mileagetracker/room/ReceiptItemEntity.java
@@ -17,7 +17,6 @@ public class ReceiptItemEntity {
     @NonNull
     public double gallonsPurchased;
 
-    @Ignore
     public ReceiptItemEntity() {}
 
     @Ignore
